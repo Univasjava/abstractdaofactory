c5c66489e6d00802153dae05138dff590d3bd60f
alfredudu <alfredudu_007@hotmail.com>
2015-08-27 20:13:15 -0300

    Alterado Abstract DAO Factory

--- src/br/edu/univas/si6/es4/abstractfactory/AbstractDAOFactory.java
+++ src/br/edu/univas/si6/es4/abstractfactory/AbstractDAOFactory.java
@@ -1,5 +1,9 @@
 package br.edu.univas.si6.es4.abstractfactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.management.RuntimeErrorException;
 
 public abstract class AbstractDAOFactory {
@@ -10,16 +14,25 @@ public abstract class AbstractDAOFactory {
 	public abstract ProdutoDAO createProdutoDAO();
 	public abstract ClienteDAO createClienteDAO();
 	
-	public static AbstractDAOFactory getfFactory(int tipo){
-		switch(tipo){
-		case TIPO_RDB:
-			return new RDBDAOFactory();
-		case TIPO_XML:
-			return new XMLDAOFactory();
-		default:
-			throw new RuntimeException("Tipo Invalido.");
+	public static AbstractDAOFactory getfFactory(){
+		String fileName = "daofactory.properties";
+		InputStream is = AbstractDAOFactory.class.getClassLoader().getResourceAsStream(fileName);
+		if(is == null) {
+			throw new RuntimeException("Recurso não Encontrado.");
 		}
+		Properties props = new Properties();
+		try {
+			props.load(is);
+		String type = props.getProperty("daofactory.factorytype");
+		if("RDB".equalsIgnoreCase(type)){
+			return new RDBDAOFactory();
+		} else if ("XML".equalsIgnoreCase(type)){
+			return new XMLDAOFactory();
+		} else {
+			throw new RuntimeException("Tipo não suportado: " + type);
+		}
+	} catch (IOException e) {
+		throw new RuntimeException("Não foi possível ler as propriedadades.", e);
 	}
-	
-	
+	}
 }
